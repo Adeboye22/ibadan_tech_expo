ac1d1d31e297b66644fb7eab8edb178ef44d83b1
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-06-16 04:36:35 +0100

    Ib tech

--- src/componentsByAdeboye/Prizes.jsx
+++ src/componentsByAdeboye/Prizes.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Prizes.css'
+import Laptop from '../assets/prizes/laptop.png'
+import Girlie from '../assets/prizes/girlie.png'
+import Watch from '../assets/prizes/watch.png'
 
 const Prizes = () => {
   return (
     <div className='prizes'>
-      <h1>prizes</h1>
+        <div className="prizes-col-1">
+            <h1 className='tech-prizes'>Tech Prizes Galore</h1>
+            <p className="tech-prizes-text">By obtaining a ticket for the Ibadan Tech Expo event,<br /> you enter the chance to win incredible tech prizes,<br /> including laptops, phones, AirPods,<br /> smartwatches, 5G internet mifis, headsets, VR sets, wireless mice, and televisions.</p>
+        </div>
+        <div className="prizes-col-2">
+            <img src={Laptop} alt="" className="img-laptop" />
+            <img src={Girlie} alt="" className="img-girlie" />
+            <img src={Watch} alt="" className="img-watch" />
+        </div>
+
     </div>
   )
 }
