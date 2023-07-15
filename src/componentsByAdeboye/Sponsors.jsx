f19d2aacc318820e0b619bd1567c6579fa4617f0
CryptoVmD <93841754+godlevels@users.noreply.github.com>
2023-07-16 00:39:11 +0100

    up

--- src/componentsByAdeboye/Sponsors.jsx
+++ src/componentsByAdeboye/Sponsors.jsx
@@ -7,7 +7,7 @@ import MTNLogo from '../assets/sponsors/mtn.svg'
 const Sponsors = () => {
     return (
       <div className='sponsors'>
-        <h1 className='mediaPartner-h1'>Partners</h1>
+        <h1 className='mediaPartner-h1'>Sponsors</h1>
         <Marquee>
         <div className="mediaPartners">
           <img src={MTNLogo} alt="" />
