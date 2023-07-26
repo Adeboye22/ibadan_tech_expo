c43c00cc219fcd34481ad36603d06f31fd209ad8
CryptoVmD <93841754+godlevels@users.noreply.github.com>
2023-07-26 22:41:04 +0100

    updated

--- src/componentsByAdeboye/MediaPartners.jsx
+++ src/componentsByAdeboye/MediaPartners.jsx
@@ -2,6 +2,9 @@ import './MediaPartners.css'
 import Marquee from 'react-fast-marquee'
 import CrestLogo from '../assets/mediaPartners/CrestLogo.svg'
 import Honour from '../assets/mediaPartners/hfm.svg'
+import AwaLogo from '../assets/mediaPartners/awa2.jpg'
+import DukLogo from '../assets/mediaPartners/duk2.jpg'
+import PunLogo from '../assets/mediaPartners/pun2.jpg'
 
 const MediaPartners = () => { 
   return (
@@ -11,8 +14,9 @@ const MediaPartners = () => {
       <div className="mediaPartners">
         <img src={CrestLogo} alt="" />
         <img src={Honour} alt="" />
-        <img src={CrestLogo} alt="" />
-        <img src={Honour} alt="" />
+        <img className='h-[2.5rem] md:h-[4.5rem] mt-1 md:mt-3 flex items-center' src={AwaLogo} alt="" />
+        <img className='h-[2.5rem] md:h-[4.5rem] mt-1 md:mt-3 flex items-center' src={DukLogo} alt="" />
+        <img className='h-[2.5rem] md:h-[4.5rem] mt-1 md:mt-3 flex items-center' src={PunLogo} alt="" />
       </div>
       </Marquee> 
     </div>
--- src/componentsByAdeboye/Sponsors.jsx
+++ src/componentsByAdeboye/Sponsors.jsx
@@ -1,6 +1,8 @@
 import Marquee from 'react-fast-marquee'
 import './Sponsors.css'
-import MTNLogo from '../assets/sponsors/mtn.svg'
+import KetLogo from '../assets/sponsors/ket2.jpg'
+import DniLogo from '../assets/sponsors/dni3.jpg'
+import OhLogo from '../assets/sponsors/oh2.jpg'
 
 
 
@@ -10,8 +12,10 @@ const Sponsors = () => {
         <h1 className='mediaPartner-h1'>Sponsors</h1>
         <Marquee>
           <div className="mediaPartners">
-            <img src={MTNLogo} alt="" />
-            <img src={MTNLogo} alt="" />
+            {/* <img src={MTNLogo} alt="" /> */}
+            <img className='h-[2rem] md:h-[4rem] mt-6 md:mt-0' src={KetLogo} alt="" />
+            <img className='h-[2rem] md:h-[4rem] mt-6 md:mt-0' src={DniLogo} alt="" />
+            <img className='h-[2rem] md:h-[4rem] mt-6 md:mt-0' src={OhLogo} alt="" />
           </div>
         </Marquee> 
       </div>
