87a4f44ecdfb3332ef2a9dc766d04d5882af50f9
CryptoVmD <93841754+godlevels@users.noreply.github.com>
2023-08-25 15:09:10 +0100

    updated

--- src/componentsByAdeboye/MediaPartners.jsx
+++ src/componentsByAdeboye/MediaPartners.jsx
@@ -1,6 +1,5 @@
 import './MediaPartners.css'
 import Marquee from 'react-fast-marquee'
-import CrestLogo from '../assets/mediaPartners/CrestLogo.svg'
 import Honour from '../assets/mediaPartners/hfm.svg'
 import BeatLogo from '../assets/mediaPartners/beat.svg'
 import PunLogo from '../assets/mediaPartners/punc.svg'
@@ -19,7 +18,6 @@ const MediaPartners = () => {
       <h1 className='mediaPartner-h1'>Media Partners</h1>
       <Marquee>
       <div className="mediaPartners">
-        <img src={CrestLogo} alt="" />
         <img src={Honour} alt="" />
         <img src={BeatLogo} alt="" />
         <img src={PunLogo} alt="" /> 
